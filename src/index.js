3d519fbf069767b511cc079dd06bfc1095c96eed
Tomas Schuster <ts2525@hotmail.com>
2021-09-14 19:49:50 -0300

    Agregado el Administrador

--- src/index.js
+++ src/index.js
@@ -1,24 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { BrowserRouter as Router, Route, Switch, Redirect  } from 'react-router-dom';
+
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
 import Home from './components/Home';
 
- 
+// admin
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./assets/css/animate.min.css";
+import "./assets/scss/light-bootstrap-dashboard-react.scss?v=2.0.0";
+import "./assets/css/demo.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
+import AdminLayout from "layouts/Admin.js";
+
 ReactDOM.render(
   <Router>
     <Switch>
+      <Route path="/admin" render={(props) => <AdminLayout {...props} />} />
       <Route path="/home" component={Home} />
       <Route path="/signup" component={SignUp} />
       <Route path="/" component={SignIn} />
-
     </Switch>
   </Router>,
   document.getElementById('root')
 );
+
+// <Redirect from="/admin" to="/admin/dashboard" />
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
